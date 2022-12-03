ed6e3947b858eb2161c58ea6e6f9c467f41139fe
Jiri Petrnousek <jiri.petrnousek@centrum.cz>
2022-12-03 18:48:55 +0000

    Day 3

---
+++ Days/Day03.fs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode2022.Days
+
+open AdventOfCode2022.Inputs
+open Xunit
+open Xunit.Abstractions
+open FsUnit.Xunit
+
+module Day03 =
+
+    let [<Literal>] Letters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let letterArray = Letters.ToCharArray()
+
+    let priority (letter : char) = System.Array.IndexOf(letterArray, letter)
+
+    let commonItemPriority (row : string) =
+        let chars = row.ToCharArray()
+        let half = (chars |> Array.length) / 2
+        let left, right = chars |> Array.splitAt half
+        let commonChar =
+            left
+            |> Array.find (fun leftChar -> right |> Array.exists (fun rightChar -> rightChar = leftChar))
+        commonChar
+        |> priority
+
+    let part1 input =
+        input
+        |> Array.map commonItemPriority
+        |> Array.sum
+
+    let groupBadgeItemPriority (rows : string[]) =
+        let sets = rows |> Array.map (fun row -> row.ToCharArray() |> Set.ofArray)
+        let badge = sets |> Set.intersectMany
+        let char = badge |> Seq.head
+        char |> priority
+
+    let part2 input =
+        input
+        |> Array.chunkBySize 3
+        |> Array.map groupBadgeItemPriority
+        |> Array.sum
+
+
+    type Tests(output:ITestOutputHelper) =
+        [<Fact>]
+        let testPart1() =
+            Inputs03.Test |> parseInput |> part1 |> should equal 157
+
+        [<Fact>]
+        let taskPart1() =
+            Inputs03.Task |> parseInput |> part1 |> string |> output.WriteLine
+
+        [<Fact>]
+        let testPart2() =
+            Inputs03.Test |> parseInput |> part2 |> should equal 70
+
+        [<Fact>]
+        let taskPart2() =
+            Inputs03.Task |> parseInput |> part2 |> string |> output.WriteLine
